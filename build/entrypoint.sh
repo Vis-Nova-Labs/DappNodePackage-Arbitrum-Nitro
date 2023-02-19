4cce5dea49266673ffa9af645f6721f215878df1
Voss <voss@visnovalabs.io>
2023-02-18 21:48:14 -0600

    integrate snapshot URL + StakersUI

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Function to check if database needs to be initialized
+if [ ! -f /home/user/.arbitrum/arb1/nitro/l2chaindata/ancient ]; then
+    echo "Downloading initial snapshot, this may take a while..."
+    # Concatenate EXTRA_OPTS with the --init.url flag
+    EXTRA_OPTS="--init.url=https://snapshot.arbitrum.io/mainnet/nitro.tar ${EXTRA_OPTS}"
+fi
+
 case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
 "geth.dnp.dappnode.eth")
     HTTP_RPC="http://geth.dappnode:8545"
@@ -21,7 +28,10 @@ esac
 
 exec nitro --l1.url=$HTTP_RPC \
     --l2.chain-id=42161 \
-    --http.addr="0.0.0.0" \
-    --http.corsdomain "*" \
-    --http.vhosts "*" \
+    --http.addr=0.0.0.0 \
+    --http.corsdomain=* \
+    --http.vhosts=* \
+    --ws.port=8548 \
+    --ws.origins=* \
+    --ws.addr=0.0.0.0 \
     $EXTRA_OPTS
