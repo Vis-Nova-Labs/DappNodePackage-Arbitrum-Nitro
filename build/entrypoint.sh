25abee9a5898dfa18f89f1af5bd26ce0c67267ec
Voss <voss@visnovalabs.io>
2024-03-23 20:27:16 -0500

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -49,15 +49,15 @@ case $_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET in
 esac
 
 if ${ETH_EL_RPC_SOURCE} === Global ; then
-    ETH_API=$EL_API
+    ETH_API=${EL_API}
 else
-    ETH_API=$EL_CUSTOM_RPC
+    ETH_API=${EL_CUSTOM_RPC}
 fi
 
 if ${ETH_CL_RPC_SOURCE} === Global ; then
-    BEACON_API=$CL_API
-elif ${ETH_CL_RPC_SOURCE} === "Custom" ; then
-    BEACON_API=$CL_CUSTOM_RPC
+    BEACON_API=${CL_API}
+elif ${ETH_CL_RPC_SOURCE} === Custom ; then
+    BEACON_API=${CL_CUSTOM_RPC}
 else
     BEACON_API="https://checkpoint-sync.dappnode.io"
 fi
